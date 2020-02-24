669da610e05ce71757ed907ac5f16a383fd184e4
OptimusRahul <rockerahul97@gmail.com>
2020-02-24 13:58:32 +0530

    added OrderComponent CheckoutContainer OrdersContainer

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    console.log(props);
     let transformedIngredients = Object.keys( props.ingredients )
         .map( igKey => {
             return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
@@ -26,7 +28,7 @@ const burger = (props) => {
     );
 };
 
-export default burger;
+export default withRouter(burger);
 
 /*
 [1,2,3,4] .map(el => return el) [1,2,3,4]
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,6 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from'../../axios-orders';
 
-
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -34,6 +33,7 @@ class BurgerBuilder extends Component{
    }
 
    componentDidMount(){
+       console.log(this.props);
        axios.get('https://react-my-burger-1fa43.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -98,32 +98,16 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        this.setState({loading: true});
-
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer : {
-                name: 'Rahul',
-                address: {
-                    street: 'Test street',
-                    zipcode: '560035',
-                    country: 'India'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-
-        axios.post('/orders.json',order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});  
-            })
-            .catch(error => {
-                 this.setState({loading: false, purchasing: false});  
-            });
-
+        queryParams.push('price='+this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
     render() {
         const disabledInfo = {
